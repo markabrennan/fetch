73656b5f76eeb1eb50ad6c2767e6515f26e73191
markabrennan <markangusbrennan@gmail.com>
2021-01-26 18:08:30 -0500

    Initial commit with first cut of working code.

---
+++ src/driver.py
@@ -0,0 +1,75 @@
+"""
+driver.py
+
+Main driver code for running, processing, and testing.
+1) Check environment variables
+2) Instantiate a ConfigMgr object to fetch config, including:
+    - stop words for text processing
+    - data file path
+3) Ingest text from each file into doc variable
+4) Derive core words from each doc
+5) Run comparison scoring function on the two sets of core words
+    and log results, as well as emit to standard error (console)
+"""
+
+import sys
+import logging
+from config_mgr import ConfigMgr
+from text_processor import ingest_text
+from text_processor import derive_core_words
+from text_processor import compare_texts
+
+
+def main(file1, file2, config_src=None):
+    """ Main routine drives the work.
+    Args:       
+        file1 and file2: filename strings
+        config_src: location of configuration
+    Returns:    Zero for success; -1 for exception (for external callers)
+    """
+    cfg = ConfigMgr(config_src)
+    logging.info('Driver: About to ingest and process texts.')
+
+    try:
+        stop_words = cfg.get('STOPWORDS')
+        data_path = cfg.get('DATA_DIR')
+
+        # ingest text from each file
+        doc1 = ingest_text(data_path + '/' + file1)
+        doc2 = ingest_text(data_path + '/' + file2)
+
+        # get core words - remove stop words and punctuation
+        doc1_words = derive_core_words(doc1, stop_words)
+        doc2_words = derive_core_words(doc2, stop_words)
+
+        # run comparison metrics on the words of the two texts
+        score = compare_texts(cfg, doc1_words, doc2_words)
+        logging.info(f'text comparison score:  {score}')
+        sys.stderr.write(f'text comparison score:  {score}\n')
+        sys.stderr.flush()
+
+    except Exception as e:
+        logging.critical(f'Process failure - exception:  {e.msg}')
+        sys.stderr.write(f'Process failure - exception:  {e.msg}\n')
+        sys.stderr.flush()
+        exit(-1)
+
+    # we've successfully concluded all processing so exit 0
+    return 0
+
+
+if __name__ == "__main__":
+    # Quick sanity check on arguments
+    if len(sys.argv) < 3:
+        # sys.stderr.write(f'usage: text_compare [file1] [file2]\n')
+        # sys.stderr.flush()
+        # sys.exit(-1)
+        file1 = 'sample1.txt'
+        file2 = 'sample3.txt'
+    else:
+        file1 = sys.argv[1]
+        file2 = sys.argv[2]
+
+    ret_val = main(file1, file2, config_src='config/config.json')
+
+    sys.exit(ret_val)
---
+++ src/text_processor.py
@@ -0,0 +1,82 @@
+"""
+Draft code to process text
+"""
+
+import logging
+import string
+import re
+import sys
+from collections import Counter
+
+
+def derive_core_words(text, stopwords):
+    """ Given a list of stop words, extract all other
+    "important" words from a given text document.
+    Args:
+        text:   string containing entire text document to process
+        stopwords:  list of stop words (from NLTK English stop words set)
+    Returns:    
+        core_word_list: List of core words in doc
+    """
+    punc_pat = re.compile(r'[' + string.punctuation + ']') 
+    core_word_list = [] 
+    for word in text.split():
+        word = word.lower()
+        word = punc_pat.sub('', word)
+        if word not in stopwords:
+            core_word_list.append(word)
+    return core_word_list
+
+
+def compare_texts(cfg, text1, text2):
+    """ Do the work of comparing two texts. This function delegates
+    to a configurable function to do the work, dynamically fetching
+    the function at runtime.
+    Args:
+        cfg:    ConfigMgr object, to use to fetch function name
+        text1 & text2:  text lists to pass to the comparison function
+    """
+    # get comparison function name from config
+    comp_func_name = cfg.get('COMP_FUNCTION')
+    # get our current module name
+    cur_module = sys.modules[__name__]
+    # fetch function object from module
+    comparison_func = getattr(cur_module, comp_func_name)
+
+    # do the real work
+    return comparison_func(text1, text2)
+
+
+def ingest_text(file_src):
+    """ Ingest text from a given filename.
+    Args:
+        file_src:   string representing file path
+    Returns:
+        buffer containing text
+    """
+    buf = ''
+    with open(file_src, 'r') as text_file:
+        buf = text_file.read()
+    return buf.strip()
+
+
+def get_jaccard_sim(words1, words2):
+    """
+    Implement the NLP Jaccard similarity score, which
+    defines the similarity of two texts as the quotient 
+    of their intersection and their union.
+    Args:
+        words1: list of words comprising first text
+        words2: list of words comprising second text
+    Returns:
+        Rounded (4 places) Jaccard score 
+
+    """
+
+    # get unique words only, then do set operations
+    a = set(words1)
+    b = set(words2)
+    c = a.intersection(b)
+
+    logging.info(f'words in common: {c} | num unique words: {len(a) + len(b) - len(c)}')
+    return round(float(len(c)) / (len(a) + len(b) - len(c)), 4)
